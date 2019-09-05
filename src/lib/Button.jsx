3d1b244f20cb492b90cacf21786beaf22eb75642
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-09-05 15:14:05 +0200

    ReactModal

---
+++ src/lib/Button.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.button`
+    display: block;
+    border: 1px solid #666;
+    background: #eee;
+    margin: 5px 0;
+    border-radius: 4px;
+    padding: 5px 10px;
+
+    :active {
+        background: #ddd;
+    }
+`
---
+++ styleguide.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+
+module.exports = {
+    title: 'ForrestJS Styleguide',
+    styleguideDir: 'docs',
+
+    /**
+     * Include all the pure component examples here:
+     */
+    components: [
+        'src/components/ReactModal/ReactModal.jsx',
+    ],
+
+    /**
+     * Customize the styleguide appearance.
+     */
+    styles: {
+        Section: {
+            root: {
+                marginBottom: '15vh',
+            },
+        },
+    },
+
+    /**
+     * Inject utility components into the Styleguide context
+     * this is really useful for providing a component with
+     * some sort of state or routing capabilities
+     */
+    context: {
+        Button: path.join(__dirname, 'src/lib/Button'),
+    },
+
+    /**
+     * Inject the default CSS.
+     * You may need to add more of those, but it's likely that you will
+     * prefer to go the `styled-components` way.
+     */
+    require: [
+        path.join(__dirname, 'src/index.css'),
+    ],
+
+    /**
+     * CRA provides a fully functioning webpack config that is good for
+     * the `/components` folder but doesn't work in any other context
+     * out of the box!
+     *
+     * The trick is to simply provide this configuration back to Styleguidist.
+     */
+    webpackConfig: require('react-scripts-rewired/config/webpack.config.dev.extend'),
+}
